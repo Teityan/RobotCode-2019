1bc3bfeab40b9ddfe36a72e6b997f5c2b5175e78
YukiTerasaki <tenko0316yuki1@gmail.com>
2019-01-19 21:17:14 +0900

    function list

--- src/main/java/frc/robot/Lift.java
+++ src/main/java/frc/robot/Lift.java
@@ -11,4 +11,33 @@ package frc.robot;
  * Add your docs here.
  */
 public class Lift {
+    /*
+    <モーター>
+    モーターの速度を変更する
+    set(数字);
+    数字:-1.0~+1.0
+    <エンコーダー>
+    getSpeed()
+        速度を得る
+    getHeight()    
+        位置を得る
+    cmで
+
+    <PID>
+    enablePID()
+        PIDを有効にする
+    disablePID()
+        PIDを無効にする
+    setSetpoint(double height)
+    setSetpoint(LiftPosition position)
+        grubberの高さを指定する
+        disableだったら動かない
+        セットポイントの数:
+            カーゴシップのハッチ付ける位置
+            カーゴシップのボール入れる位置
+            ロケットのハッチ付ける位置
+                1~2段目
+            ロケットのボール入れる位置
+                1~2段目
+    */
 }
